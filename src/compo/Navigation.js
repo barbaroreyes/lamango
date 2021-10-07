64029aec66274e9d6467aad5faf703a86b5f3ad0
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-10-07 10:08:26 -0500

    added-nav-sandwish

--- src/compo/Navigation.js
+++ src/compo/Navigation.js
@@ -1,18 +1,15 @@
 import React from 'react';
+import NavigationMovil from './navigation-movil';
 import {Link} from 'react-router-dom'
 const Navigation = () => {
   return (
     <div>
         <nav className="navigation">
-        <Link to ='/'>Home</Link>
+         <NavigationMovil/>
+        {/* <Link to ='/'>Home</Link>
         <Link to ='/categorias'>Categorias</Link>
         <Link to ='/about'>About Us</Link>
-        <Link to ='/contact'>Contacto</Link>
-
-        
-       
-
-
+        <Link to ='/contact'>Contacto</Link> */}
         </nav>
     </div>
   );
---
+++ src/compo/navigation-movil.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigationmovil = () => {
+  return (
+    <div className="navigation-circle">
+       <div className="line"></div>
+       <div className="line"></div>
+       <div className="line"></div>
+    </div>
+  );
+}
+
+export default Navigationmovil;
